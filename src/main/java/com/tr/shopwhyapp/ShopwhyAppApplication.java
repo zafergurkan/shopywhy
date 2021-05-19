495242af24c85155a4a6d6704f476ef1c86f7742
Zafer Gürkan <zafergurkan@outlook.com>
2021-05-19 15:30:50 +0300

    create project

---
+++ src/main/java/com/tr/shopwhyapp/ShopwhyAppApplication.java
@@ -0,0 +1,13 @@
+package com.tr.shopwhyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopwhyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopwhyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tr/shopwhyapp/ShopwhyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tr.shopwhyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopwhyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
